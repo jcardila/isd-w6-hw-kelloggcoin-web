fe7718b3b70a1a4b11813e34a563f734922f0acb
jcardila <andrea.rueda@kellogg.northwestern.edu>
2022-02-21 01:00:07 +0000

    Balances y Fun Table Ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    def transactions
+        Transaction.where({ company_id: self.id })
+        Transaction.where({ to_user_id: self.id }).sum(:amount)
+    end
 end
